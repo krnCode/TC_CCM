7fbf1572a1c9eff6d607f6c679fac29eeabfe1fc
krn <contadorkrn@gmail.com>
2026-01-07 10:59:35 -0300

    DEV: Finalizar desenvolvimento do tratamento de dados do historico de vendas para a camada trusted

--- src/etl/scripts/1-trusted/historico-venda-pecas-trusted.py
+++ src/etl/scripts/1-trusted/historico-venda-pecas-trusted.py
@@ -9,6 +9,8 @@ import polars as pl
 
 from pathlib import Path
 from datetime import datetime
+from src.etl.schemas.historico_venda_pecas_schema import HISTORICO_VENDA_PECAS_SCHEMA
+from src.ferramentas.funcoes_suporte import salvar_parquet
 
 
 # region ----- Caminho Arquivo Raw -----
@@ -23,6 +25,19 @@ RAW_FOLDER_PATH: Path = (
 RAW_DATA_FILE = os.listdir(RAW_FOLDER_PATH)[0]
 
 RAW_FILE_PATH = RAW_FOLDER_PATH / RAW_DATA_FILE
+# endregion
+
+
+# region ----- Caminho Arquivo Trusted -----
+TRUSTED_FOLDER_PATH: Path = (
+    Path(__file__).parent.parent.parent.parent
+    / "etl"
+    / "data"
+    / "1-trusted"
+    / "historico-venda-pecas"
+)
+
+TRUSTED_FOLDER_PATH.mkdir(parents=True, exist_ok=True)
 # endregion
 
 
@@ -63,6 +78,25 @@ def categorias_padrao() -> dict:
     return categorias_padrao
 
 
+# Padronizar departamentos
+def departamentos_padronizados() -> dict:
+    departamentos_padronizados: dict = {
+        "ACESSORIOS": "ACESSÓRIOS",
+        "ADMINISTRACAO": "ADMINISTRAÇÃO",
+        "ASSISTENCIA TECNICA": "ASSISTÊNCIA TÉCNICA",
+        "ASSISTENCIA TECNICA ": "ASSISTÊNCIA TÉCNICA",
+        "FUNILARIA E PINTURA": "FUNILARIA E PINTURA",
+        "OFICINA": "OFICINA",
+        "PECAS": "PEÇAS",
+        "PECAS ATACADO": "PEÇAS",
+        "PECAS BALCAO": "PEÇAS",
+        "PECAS VAREJO": "PEÇAS",
+        "VEICULOS NOVOS": "VEÍCULOS NOVOS",
+    }
+
+    return departamentos_padronizados
+
+
 # endregion
 
 
@@ -83,7 +117,7 @@ df_raw = df_raw.with_columns(pl.col("Lucro_da_Venda").cast(pl.Float64))
 df_raw = df_raw.with_columns(pl.col("Margem_da_Venda").cast(pl.Float64))
 
 
-# Remover linhas duplicadas
+# Destacar linhas duplicadas, parcialmente duplicadas
 colunas_subset: list = [
     "Cod_Concessionaria",
     "Cod_Filial",
@@ -104,7 +138,24 @@ colunas_subset: list = [
     "Estado_Brasileiro_da_Venda",
     "Macroregiao_Geografica_da_Venda",
 ]
-df_raw = df_raw.unique(subset=colunas_subset)
+
+df_raw = df_raw.with_columns(
+    pl.struct(pl.all()).is_duplicated().alias("Linha_Duplicada")
+)
+
+
+df_raw = df_raw.with_columns(
+    pl.struct(colunas_subset).is_duplicated().alias("Linha_Duplicada_Parcial")
+)
+
+df_raw = df_raw.with_columns(
+    pl.when(pl.col("Linha_Duplicada"))
+    .then(pl.lit("DUPLICADO"))
+    .when(pl.col("Linha_Duplicada_Parcial"))
+    .then(pl.lit("DUPLICADO PARCIALMENTE"))
+    .otherwise(pl.lit("OK"))
+    .alias("Status_Duplicidade")
+)
 
 
 # Padronizar as categorias
@@ -112,6 +163,14 @@ df_raw = df_raw.with_columns(
     pl.col("Categoria_da_Peca")
     .replace(categorias_padrao())
     .alias("Categoria_da_Peca_Padronizada")
+)
+
+
+# Padronizar departamentos
+df_raw = df_raw.with_columns(
+    pl.col("Departamento_da_Venda")
+    .replace(departamentos_padronizados())
+    .alias("Departamento_da_Venda_Padronizada")
 )
 
 
@@ -142,8 +201,98 @@ df_raw = df_raw.with_columns(
     ]
 )
 
-# Ajustar valor de custo
+
+# Recalcular lucro da venda
+df_raw = df_raw.with_columns(
+    (pl.col("Valor_da_Venda_Ajustado") - pl.col("Custo_da_Peca_Ajustado")).alias(
+        "Lucro_da_Venda_Recalculado"
+    )
+)
+
+# Recalcular margem da venda
+df_raw = df_raw.with_columns(
+    (pl.col("Lucro_da_Venda_Recalculado") / pl.col("Valor_da_Venda_Ajustado")).alias(
+        "Margem_da_Venda_Recalculado"
+    )
+)
+
+# Ajustar margem quando o lucro for 0
+df_raw = df_raw.with_columns(
+    pl.when(pl.col("Lucro_da_Venda_Ajustado") == 0)
+    .then(0)
+    .otherwise(pl.col("Margem_da_Venda_Recalculado"))
+    .alias("Margem_da_Venda_Recalculado")
+)
+
+# Classificar valor de custo desproporcionais
+df_raw = df_raw.with_columns(
+    pl.when(pl.col("Custo_da_Peca") > 400000)
+    .then(pl.lit("VALOR DE CUSTO DESPROPORCIONAL"))
+    .otherwise(pl.lit("OK"))
+    .alias("Custo_da_Peca_Classificado")
+)
+
+
+# Classificar devoluções com lucro
+df_raw = df_raw.with_columns(
+    pl.when(
+        (pl.col("Lucro_da_Venda") > 0)
+        & (pl.col("Tipo_de_Venda_da_Peca") == "DEVOLUCAO")
+    )
+    .then(pl.lit("DEVOLUCAO COM LUCRO"))
+    .otherwise(pl.lit("OK"))
+    .alias("Lucro_da_Venda_Classificado")
+)
+
+# Classificar registros com valor de custo igual a 0
+df_raw = df_raw.with_columns(
+    pl.when(pl.col("Custo_da_Peca") == 0)
+    .then(pl.lit("VALOR DE CUSTO IGUAL A 0"))
+    .otherwise(pl.lit("OK"))
+    .alias("Custo_da_Peca_Classificado")
+)
+
+
+# Classificar linhas como CONFIÁVEIS ou NÃO CONFIÁVEIS conforme a qualidade do registro
+df_raw = df_raw.with_columns(
+    pl.when(
+        (pl.col("Status_Duplicidade") == "OK")
+        & (pl.col("Custo_da_Peca_Classificado") == "OK")
+        & (pl.col("Lucro_da_Venda_Classificado") == "OK")
+    )
+    .then(pl.lit("CONFIÁVEL"))
+    .otherwise(pl.lit("NÃO CONFIÁVEL"))
+    .alias("Confiabilidade_do_Registro")
+)
+
+
+# Renomear colunas para minusculas
+df_raw = df_raw.rename(str.lower)
+
 # endregion
 
-print(df_raw.columns)
+
 df_raw.write_excel(workbook="historico-venda-pecas-trusted.xlsx")
+
+
+# region ----- Salvar Arquivo Trusted -----
+# Salvar em um novo dataframe com os dados finais
+df_trusted: pl.DataFrame = pl.DataFrame(
+    data=df_raw,
+    schema=HISTORICO_VENDA_PECAS_SCHEMA,
+)
+
+time_now: datetime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+file_name_trusted: str = f"historico-venda-pecas-trusted-{time_now}.parquet"
+
+
+salvar_parquet(
+    df=df_trusted,
+    path=TRUSTED_FOLDER_PATH,
+    file_name=file_name_trusted,
+)
+
+file = os.listdir(TRUSTED_FOLDER_PATH)[0]
+test: pl.DataFrame = pl.read_parquet(source=TRUSTED_FOLDER_PATH / file)
+
+# endregion
