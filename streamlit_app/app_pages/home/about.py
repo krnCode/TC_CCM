e465bcb9d2509f7bc02fb39a790fd7d34354a430
krn <contadorkrn@gmail.com>
2026-01-07 20:06:45 -0300

    DEV: Criar página inicial do app

---
+++ streamlit_app/app_pages/home/about.py
@@ -0,0 +1,33 @@
+"""
+Página de entrada do app, com informações sobre o projeto.
+"""
+
+import streamlit as st
+
+
+# region ----- Página Config -----
+st.set_page_config(
+    page_title="Sobre",
+    layout="centered",
+)
+# endregion
+
+
+# region ----- Sobre o projeto -----
+st.title("Sobre")
+st.write(
+    """
+        Olá, tudo bem?
+
+        Esta página do Streamlit serve para apresentar os dados deste projeto, 
+        desde os dados brutos até a camada Trusted.
+
+        O objetivo é ter um painel interativo com que foi possível analisar dos 
+        dados e fornecer visualizações e informações do que foi identificado 
+        e tratado.
+
+        Para seguir, basta selecionar as páginas do menu à esquerda.
+
+        ---
+    """
+)
